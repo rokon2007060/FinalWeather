772006e889bf2e3b3edd7efc7de493098d301ba5
rokon <rokon2007060@stud.kuet.ac.bd>
2024-05-20 09:20:45 +0600

    Final commit

--- WeatheApp/app/Http/Controllers/Auth/RegisterController.php
+++ WeatheApp/app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
